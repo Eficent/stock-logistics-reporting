b9c19f7714865a01dc4cf52eb76a418694ed1459
Núria Martín Xifré <36236518+NuriaMartinXifre@users.noreply.github.com>
2019-11-27 16:56:33 +0100

    Update stock_picking_product_label/__manifest__.py
    Co-Authored-By: Héctor Villarreal Ortega <32061121+hveficent@users.noreply.github.com>

--- stock_picking_product_label/__manifest__.py
+++ stock_picking_product_label/__manifest__.py
@@ -1,4 +1,4 @@
-# Copyright 2019 Eficent <patrickraymondwilson@gmail.com>
+# Copyright 2019 Eficent Business and IT Consulting Services S.L.
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
 
 {
